7b35cdc1a8f8b27f739b3eeff83106b8634d3399
Adams-Ghub <bamfoadamsf@gmail.com>
2023-08-25 17:36:14 +0000

    Add styling to pages and updated router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './components/sessions/register';
 import Login from './components/sessions/login';
+import Motorcycles from './components/pages/motorcycles';
+import Reservations from './components/pages/reservations';
+import AddMotorcycle from './components/pages/add-motorcycle';
+import Delete from './components/pages/delete';
+import Reserve from './components/pages/reserve';
 
 function App() {
   return (
@@ -9,6 +14,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Register />} />
         <Route path="/user/login" element={<Login />} />
+        <Route path="/motorcycles" element={<Motorcycles />} />
+        <Route path="/reserve" element={<Reserve />} />
+        <Route path="/reservations" element={<Reservations />} />
+        <Route path="/add-motorcycle" element={<AddMotorcycle />} />
+        <Route path="/delete" element={<Delete />} />
       </Routes>
     </Router>
   );
