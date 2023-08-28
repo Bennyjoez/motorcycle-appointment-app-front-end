f07a75d72d6862a95cd91b58249a19457d277d92
Adams-Ghub <bamfoadamsf@gmail.com>
2023-08-28 09:53:18 +0000

    Add styling to navPanel

--- src/components/nav-panel.js
+++ src/components/nav-panel.js
@@ -7,19 +7,19 @@ const NavPanel = () => {
     <nav className="nav-panel-main-container">
       <div className="logo-container">logo</div>
       <ul className="nav-links-container">
-        <li className="nav-link">
+        <li className="nav-link-container">
           <Link to="/motorcycles">Motorcyles</Link>
         </li>
-        <li className="nav-link">
-          <Link to="/reserve">Reserve</Link>
+        <li className="nav-link-container">
+          <Link id='link' to="/reserve">Reserve</Link>
         </li>
-        <li className="nav-link">
+        <li className="nav-link-container">
           <Link to="/reservations">Reservations</Link>
         </li>
-        <li className="nav-link">
+        <li className="nav-link-container">
           <Link to="/add-motorcycle">Add Motorcycle</Link>
         </li>
-        <li className="nav-link">
+        <li className="nav-link-container">
           <Link to="/delete">Delete</Link>
         </li>
       </ul>
