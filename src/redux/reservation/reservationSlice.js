b907c8f7ecbfa14c2f8f97e9fe1f9f21bfcb2385
Emmanuel <emmanuelnwoye12@gmail.com>
2023-08-31 13:21:11 +0100

    Add functions to post and get reservations

--- src/redux/reservation/reservationSlice.js
+++ src/redux/reservation/reservationSlice.js
@@ -4,17 +4,42 @@ import axios from 'axios';
 // const userId = JSON.parse(localStorage.getItem('userId'));
 // const RESERVATION_URL = `http://localhost:3000/api/users/${userId}/reservations`;
 
-export const postReservation = createAsyncThunk('postReservation', async (data) => {
+export const postReservation = createAsyncThunk(
+  'postReservation',
+  async (data) => {
+    const userId = JSON.parse(localStorage.getItem('user')).id;
+    const RESERVATION_URL = `http://localhost:3000/api/users/${userId}/reservations`;
+    const response = await axios.post(RESERVATION_URL, data);
+    // console.log('This is your response!');
+    return response.data;
+    // const responseData = await response.json();
+    // if (response.status < 200 || response.status >= 300) {
+    //   throw new Error('Failed to add reservation');
+    // }
+    // return responseData;
+  },
+);
+
+export const getReservations = createAsyncThunk('getReservations', async () => {
   const userId = JSON.parse(localStorage.getItem('user')).id;
   const RESERVATION_URL = `http://localhost:3000/api/users/${userId}/reservations`;
-  const response = await axios.post(RESERVATION_URL, data);
-  // console.log('This is your response!');
-  return response.data;
-  // const responseData = await response.json();
-  // if (response.status < 200 || response.status >= 300) {
-  //   throw new Error('Failed to add reservation');
-  // }
-  // return responseData;
+  const response = await axios.get(RESERVATION_URL, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+  // console.log(response.data);
+  // return response.data;
+  const { data } = response;
+  const reservation = data.map((reservation) => ({
+    id: reservation.id,
+    motorcycle_id: reservation.motorcycle_id,
+    user_id: reservation.user_id,
+    date: reservation.date,
+    city: reservation.city,
+    status: reservation.status,
+  }));
+  return reservation;
 });
 
 const initialState = {
@@ -33,8 +58,9 @@ const reservationSlice = createSlice({
       state.creationMsg = action.payload;
     },
     setRemoveReservation: (state, action) => {
-      state.reservation = state.reservation
-        .filter((reservation) => reservation.motorcycle_id !== action.payload);
+      state.reservation = state.reservation.filter(
+        (reservation) => reservation.motorcycle_id !== action.payload,
+      );
     },
   },
   extraReducers: (builder) => {
@@ -52,6 +78,18 @@ const reservationSlice = createSlice({
         ...state,
         error: action.payload,
         creationMsg: action.error.message,
+      }))
+      .addCase(getReservations.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(getReservations.fulfilled, (state, action) => {
+        state.reservation = action.payload;
+        state.loading = false;
+        // state.reservationList = action.payload;
+      })
+      .addCase(getReservations.rejected, (state, action) => ({
+        ...state,
+        error: action.payload,
       }));
   },
 });
