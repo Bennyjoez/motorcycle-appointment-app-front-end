e40c847707a986ba6c1c410703a3cadd838491a4
Emmanuel <emmanuelnwoye12@gmail.com>
2023-08-31 13:20:42 +0100

    Add functions and form to create new reservation

--- src/components/pages/reserve.js
+++ src/components/pages/reserve.js
@@ -1,7 +1,143 @@
-import React from 'react';
-import NavPanel from '../nav-panel';
+import { useSelector, useDispatch } from 'react-redux';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+import {
+  createMsgAction,
+  postReservation,
+} from '../../redux/reservation/reservationSlice';
 import '../../stylesheets/reserve.css';
 import Navbar from '../navbar';
+
+const Reservations = () => {
+  // const { motorcycles } = useSelector((state) => state.motorcycles);
+  const createmsg = useSelector((state) => state.sessions);
+  const reservationData = useSelector((state) => state.reservations);
+  console.log(reservationData);
+  const user = JSON.parse(window.localStorage.getItem('user'));
+  const isLoggedIn = JSON.parse(window.localStorage.getItem('logged_in'));
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { chosenMotorcycleId = -1 } = location.state || {};
+
+  const [motorcycleIdState, setMotorcycleIdState] = useState(chosenMotorcycleId);
+  const [date, setDate] = useState('');
+  const [city, setCity] = useState('');
+  const [status, setStatus] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [created, setCreated] = useState(false);
+
+  useEffect(() => {
+    if (!isLoggedIn) {
+      setTimeout(() => {
+        navigate('/');
+      }, 2000);
+    }
+
+    if (createmsg === 'success') {
+      setCreated(true);
+      setErrorMessage('');
+      dispatch(createMsgAction());
+      setTimeout(() => {
+        navigate('/reservations');
+      }, 2500);
+    } else {
+      setErrorMessage(
+        "Oops! Reservation couldn't be created. Can't reserve the same motorcycle on the same day",
+      );
+    }
+    if (createmsg === "Reservation couldn't be created.") {
+      setErrorMessage(
+        "Oops! Reservation couldn't be created. Can't reserve the same motorcycle on the same day.",
+      );
+      dispatch(createMsgAction());
+    }
+  }, [createmsg, created, dispatch, isLoggedIn, navigate]);
+
+  if (!isLoggedIn) {
+    return (
+      <div className="popup-message">
+        <p>Please log in to access this page</p>
+      </div>
+    );
+  }
+
+  const cities = [
+    // Cities from Nigeria
+    'Lagos',
+    'Abuja',
+    'Kano',
+    'Ibadan',
+    'Port Harcourt',
+    'Benin City',
+    'Kaduna',
+    'Enugu',
+    'Abeokuta',
+    'Onitsha',
+
+    // Cities from Kenya
+    'Nairobi',
+    'Mombasa',
+    'Kisumu',
+    'Nakuru',
+    'Eldoret',
+    'Thika',
+    'Malindi',
+    'Kakamega',
+    'Meru',
+    'Nyeri',
+
+    // Cities from Ghana
+    'Accra',
+    'Kumasi',
+    'Tamale',
+    'Takoradi',
+    'Cape Coast',
+    'Sekondi-Takoradi',
+    'Ho',
+    'Koforidua',
+    'Wa',
+    'Sunyani',
+
+    // Cities from India
+    'Mumbai',
+    'Delhi',
+    'Bangalore',
+    'Hyderabad',
+    'Chennai',
+    'Kolkata',
+    'Pune',
+    'Ahmedabad',
+    'Jaipur',
+    'Lucknow',
+  ];
+
+  const statusOption = ['Confirmed', 'Not confirmed'];
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (
+      motorcycleIdState === -1
+      || date === ''
+      || city === ''
+      || status === ''
+    ) {
+      setErrorMessage('Please fill all fields');
+    }
+    const reservation = {
+      motorcycle_id: motorcycleIdState,
+      user_id: user,
+      date,
+      city,
+      status,
+    };
+    dispatch(postReservation(reservation));
+    navigate('/reservations');
+  };
+
+  const getCurrentDate = () => new Date().toJSON().slice(0, 10);
 
   return (
     <section className="reserve-motorcycle-page">
@@ -17,7 +153,7 @@ import Navbar from '../navbar';
 
         <form onSubmit={handleSubmit} className="reserve-form">
           <select
-            defaultValue={chosenMotorcycleId || ""}
+            defaultValue={chosenMotorcycleId || ''}
             name="motorcycle_id"
             id="motorcycle-drop-down"
             onChange={(e) => setMotorcycleIdState(e.target.value)}
@@ -73,7 +209,7 @@ import Navbar from '../navbar';
           <input type="submit" value="Book Now" />
         </form>
 
-        <div className={`popup-message ${created ? "" : "hidden"}`}>
+        <div className={`popup-message ${created ? '' : 'hidden'}`}>
           <p>Reservation has been created successfully!</p>
         </div>
       </div>
