35ce6fb1562f6b4bf20a90ad13e53986181308ea
Adams-Ghub <bamfoadamsf@gmail.com>
2023-08-31 15:48:08 +0000

    Modify the motor cards

--- src/components/motor-card.js
+++ src/components/motor-card.js
@@ -1,11 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import '../stylesheets/motor-card.css';
+import { FaFacebookF, FaTwitter } from 'react-icons/fa';
+import { ImInstagram } from 'react-icons/im';
 
-const MotorCard = ({ name, imgUrl }) => (
+const MotorCard = ({ name, imgUrl, description }) => (
   <div className="card-main-container">
-    <img src={imgUrl} alt={`${name}_image`} width={250} height={250} className="card-img" />
+    <img
+      src={imgUrl}
+      alt={`${name}_image`}
+      width={250}
+      height={250}
+      className="card-img"
+    />
     <div className="motor-name">{name}</div>
+    <p>{description}</p>
+    <div className='icon-container'>
+      <a href="#">
+        <FaFacebookF  />
+      </a>
+      <a href="#">
+        <FaTwitter />
+      </a>
+      <a href="#">
+        <ImInstagram />
+      </a>
+    </div>
   </div>
 );
 
