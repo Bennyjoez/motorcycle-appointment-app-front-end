1cf5f14e87d683457c68bee72980bb2d5dd61652
Adams-Ghub <bamfoadamsf@gmail.com>
2023-08-31 10:42:06 +0000

    Modify motorcycles.js file with the navbar component

--- src/components/pages/motorcycles.js
+++ src/components/pages/motorcycles.js
@@ -1,46 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-import NavPanel from '../nav-panel';
 import '../../stylesheets/motorcycles.css';
 import MotorCard from '../motor-card';
 import { getMotorcycles } from '../../redux/motorcycles/motorcycleSlice';
+import Navbar from '../navbar';
 
 const Motorcycles = () => {
   const dispatch = useDispatch();
   const motorcycles = useSelector((state) => state.motorcycles);
-  const [menuToggleState, setMenuToggleState] = useState('hide');
 
   useEffect(() => {
     dispatch(getMotorcycles());
   }, []);
 
-  const handleMenuToggle = () => {
-    if (menuToggleState === 'hide') {
-      setMenuToggleState('');
-    } else {
-      setMenuToggleState('hide');
-    }
-  };
   return (
     <div className="motorcycles-main-container">
-      <nav className="mobile-nav">
-        <button type="button" onClick={handleMenuToggle} className="menu-toggle">
-          &#9776;
-        </button>
-      </nav>
-      <section
-        className={`nav-container ${menuToggleState}`}
-        id="nav-container"
-      >
-        <button
-          type="button"
-          onClick={handleMenuToggle}
-          className={`close-btn ${menuToggleState}`}
-        >
-          &#10005;
-        </button>
-        <NavPanel />
+      <section className="nav-container">
+        <Navbar />
       </section>
       <section className="page-body">
         <div className="title-container">
