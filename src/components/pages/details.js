40c410ac6afdd5e469a17f0a2436b39e670ad7e2
Adams-Ghub <bamfoadamsf@gmail.com>
2023-08-31 05:57:47 +0000

    Add responsive styling to details page

--- src/components/pages/details.js
+++ src/components/pages/details.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavPanel from '../nav-panel';
-import '../../stylesheets/reserve.css';
+import '../../stylesheets/details.css';
 
-const Details = () => (
-  <div className="reserve-main-container">
-    <section className="reserve-nav-container">
-      <NavPanel />
-    </section>
-    <section className="reserve-body">This is the Details</section>
-  </div>
-);
+const Details = () => {
+  const [menuToggleState, setMenuToggleState] = useState('hide');
+
+  const handleMenuToggle = () => {
+    if (menuToggleState === 'hide') {
+      setMenuToggleState('');
+    } else {
+      setMenuToggleState('hide');
+    }
+  };
+  return (
+    <div className="details-main-container">
+      <nav className="details-mobile-nav">
+        <button
+          type="button"
+          onClick={handleMenuToggle}
+          className="details-menu-toggle"
+        >
+          &#9776;
+        </button>
+      </nav>
+      <section className={`details-nav-container ${menuToggleState}`}>
+        <button
+          type="button"
+          onClick={handleMenuToggle}
+          className={`close-btn ${menuToggleState}`}
+        >
+          &#10005;
+        </button>
+        <NavPanel />
+      </section>
+      <section className="details-body">This is the Details</section>
+    </div>
+  );
+};
 
 export default Details;
