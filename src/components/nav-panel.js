035015b225d5e747f85bbe84cd3295b9afdf44aa
Emmanuel <emmanuelnwoye12@gmail.com>
2023-09-01 11:46:42 +0100

    Add logout functionality

--- src/components/nav-panel.js
+++ src/components/nav-panel.js
@@ -1,12 +1,22 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { useNavigate, Link } from 'react-router-dom';
+import { logOut } from '../redux/sessions/sessionsSlice';
 import '../stylesheets/navpanel.css';
 
 const NavPanel = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const isLoggedIn = JSON.parse(window.localStorage.getItem('logged_in'));
+
+  const clearUserData = () => {
+    localStorage.clear();
+    dispatch(logOut());
+    navigate('/');
+  };
   return (
     <nav className="nav-panel-main-container">
-
       <div className="logo-container">logo</div>
 
       <div className="nav-links-container">
@@ -25,6 +35,17 @@ const NavPanel = () => {
         <Link to="/delete" className="nav-link">
           Delete
         </Link>
+        <div>
+          {isLoggedIn && (
+            <button
+              className="logout-btn"
+              type="button"
+              onClick={clearUserData}
+            >
+              Logout
+            </button>
+          )}
+        </div>
       </div>
     </nav>
   );
