916fcca2515ed344a73a86f3e968127fa03ebd7d
Adams-Ghub <bamfoadamsf@gmail.com>
2023-09-01 23:05:26 +0000

    Modify store to allow persistence

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
-import sessionsReducer from './sessions/sessionsSlice';
-import motorcycleReducer from './motorcycles/motorcycleSlice';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import rootReducer from './rootReducer';
 
-const store = configureStore({
-  reducer: {
-    sessions: sessionsReducer,
-    motorcycles: motorcycleReducer,
-  },
-});
-
-export default store;
+const persistConfig = { key: 'motorcycleApp', storage };
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+const store = configureStore({ reducer: { state: persistedReducer } });
+const persistor = persistStore(store);
+export { store, persistor };
